5226892f4d9a18d9ddccc560752eb9c6ebdca5ef
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-04 16:17:57 -0300

    arguments 2

--- js/defaultfunctionarguments.js
+++ js/defaultfunctionarguments.js
@@ -1,5 +1,51 @@
-function multiply (a,b) { 
-    b = b || 1;
+function multiply (a, b = randomNumber() /*a,1*/ ) { 
+    //b = b || 1;
+    //b = typeof b === 'undefined' ? 1: b;
+    //if (typeof b === 'undefined'){ b = 1;}
+    
     return a*b; }
 
-console.log(multiply(5));
+
+
+//lazy evaluation
+function randomNumber(){
+    return Math.random()*10;
+}
+
+//console.log(multiply(5));
+//console.log(randomNumber());
+
+/* Enhanced Object Literals*/
+
+/*
+var obj = {
+    prop1: 'Digital Innovation One'
+};
+*/
+
+var prop1 = 'Digital Innovation One';
+
+var obj = {
+    //prop1: prop1 
+    prop1 
+};
+
+console.log(obj);
+
+function method1() {console.log('method called');}
+var obj2 = {
+    //prop1: prop1 
+    method1 
+};
+obj2.method1();
+
+
+var obj3 = {
+    sum: function sum(a,b){return a + b;}
+};
+console.log(obj3.sum(1,5));
+
+var obj4 = {
+    sum(a,b){return a + b;}
+};
+console.log(obj4.sum(5,5));
