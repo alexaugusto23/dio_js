ed2a5b93d2cefa76dc23d131fffe54ab7810a562
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-04 10:36:03 -0300

    repeat

--- js/filter.js
+++ js/filter.js
@@ -15,4 +15,12 @@ const pets =
         type: 'fish',
         age: 1
     }
-]
+];
+
+const newPets = pets.filter((pet) => {
+    return pet.age < 5
+});
+
+console.log(pets);
+console.log("- ".repeat(20));
+console.log(newPets);
