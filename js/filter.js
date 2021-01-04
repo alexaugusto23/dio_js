e0f37ec127a60d463142dbd13df409f8cb589fd6
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-04 10:25:10 -0300

    filter

---
+++ js/filter.js
@@ -0,0 +1,18 @@
+const pets = 
+[
+    {
+        name: 'rex',
+        type: 'dog',
+        age: 10
+    },
+    {
+        name: 'miau',
+        type: 'cat',
+        age: 2
+    },
+    {
+        name: 'gulp',
+        type: 'fish',
+        age: 1
+    }
+]
