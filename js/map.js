66af6b7aaee36b5ffa3bb17a470816943178417d
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-04 11:17:36 -0300

    map

---
+++ js/map.js
@@ -0,0 +1,46 @@
+const pets = 
+[
+    {
+        type: 'dog',
+        name: 'bolinha',
+        age: 15,
+        weight: 30
+    },
+
+    {
+        type: 'cat',
+        name: 'mingal',
+        age: 6,
+        weight: 2
+    },
+
+    {
+        type: 'dog',
+        name: 'rex',
+        age: 4,
+        weight: 5
+    },
+
+    {
+        type: 'cat',
+        name: 'marron',
+        age: 2,
+        weight: 1
+    },
+
+    {
+        type: 'fish',
+        name: 'gulp',
+        age: 1,
+        weight: 0.01
+    },
+
+    {
+        type: 'horse',
+        name: 'pé de pano',
+        age: 1,
+        weight: 500
+    }
+]
+
+console.log(pets)
