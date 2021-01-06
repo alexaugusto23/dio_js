dea7f993a9351d1f83e62f221b39cc112e1f7d79
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-06 16:39:34 -0300

    symbols

---
+++ js/symbolseinterators.js
@@ -0,0 +1,59 @@
+//symbols
+//onst uniqueId = Symbol('Hello');
+//const uniqueId2 = Symbol('Hello');
+
+//const obj = {[uniqueId]: 'Hello'};
+
+//console.log(obj);
+//console.log(uniqueid === uniqueid2);
+//Symbol.iterator
+//Symbol.split
+//Symbol.toStringTag
+//Arr
+
+//const arr = [1, 2, 3, 4];
+//const str = "Digital Innovation One";
+//const it = arr[Symbol.iterator]();
+
+//for (let value of str){console.log(value);}
+
+//console.log(arr[Symbol.iterator]().next());
+
+/*
+while(true)
+{
+    let {value, done} = it.next()
+    if (done){break;}
+    console.log(value);
+}
+*/
+//console.log(it.next());
+//console.log(it.next());
+//console.log(it.next());
+//console.log(it.next());
+
+Symbol.iterator;
+
+const obj = {
+    values: [1, 2, 3, 4],
+    [Symbol.iterator](){
+        let i = 0;
+        return{
+            next: () => { 
+                i++;
+                return { 
+                    value: this.values[i -1], 
+                    done: i > this.values.length
+                };
+            }
+        };
+    }
+};
+
+const it = obj[Symbol.iterator]();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
