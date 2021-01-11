bbe483ce0bb8484dedc0a555bd4af1bed8e21d0b
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-11 18:53:31 -0300

    finalidao contador

--- debughtml/script.js
+++ debughtml/script.js
@@ -3,4 +3,22 @@ class app{
     constructor(){
         this.bind();
     }
+
+    bind(){
+        document.querySelector('.add-button').addEventListener('click', () => {
+            this.handleClick(1);
+        });
+
+        document.querySelector('.sub-button').addEventListener('click', () => {
+            this.handleClick(-1);
+        });
+    }
+
+    handleClick(val){
+        const counterEL = document.querySelector('.counter');
+        const value = parseInt(counterEL.innerText);
+        counterEL.innerText = value + val;
+    }
+
 }
+new app();
